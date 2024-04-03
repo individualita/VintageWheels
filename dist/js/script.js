39cbadbf012a65cedc64252d2b77cbde19bb9f2d
Roman <asdwka3@gmail.com>
2024-04-03 17:51:32 +0200

    added gallery section with swiper slider

---
+++ dist/js/script.js
@@ -0,0 +1,14 @@
+//swiper slider
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: "1.5",
+  spaceBetween: 30,
+  initialSlide: 2,
+  centeredSlides: true,
+  speed: 800,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.gallery__btn-next',
+      prevEl: '.gallery__btn-prev',
+    },
+  });
