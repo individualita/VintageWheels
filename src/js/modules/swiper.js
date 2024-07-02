e4036280eb63a52b5ca5ea3512c586dc6eadc859
Roman <asdwka3@gmail.com>
2024-07-02 17:29:30 +0200

    sections initially adapted for testing on a phone

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -8,6 +8,20 @@ const swiper = new Swiper('.swiper', {
 	initialSlide: 2,
 	centeredSlides: true,
 	speed: 800,
+	breakpoints: {
+		740: {
+			spaceBetween: 30,
+		},
+		640: {
+			slidesPerView: "1.3",
+			spaceBetween: 10,
+		  },
+		320: {
+			slidesPerView: 1,
+		},
+
+
+	},
 	  // Navigation arrows
 	  navigation: {
 		nextEl: '.gallery__btn-next',
